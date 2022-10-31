b0507186388a1d174f399a7d8e106c2ab6f08e81
0VLaDOS0 <112828132+0VLaDOS0@users.noreply.github.com>
2022-10-31 19:42:53 +0300

    Add files via upload

---
+++ Lesson12.sql
@@ -0,0 +1,9 @@
+1)
+SELECT SUBSTRING('Please find the substring in this string', 17, 9); 
+substring
+
+2)
+SELECT ABS (-25.76823) , SIGN ( -25.76823) , ROUND ( -25.76823 , 2) ;
+
+3)
+SELECT EXTRACT (MONTH FROM CURRENT_DATE ( ) ) ;
